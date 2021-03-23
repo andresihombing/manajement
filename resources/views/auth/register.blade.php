6fb9635508f84459572a611a39aafe9eef3e9e50
Alfred Chrisdianto Simanjuntak <if416010@studens.del.ac.id>
2021-03-23 20:12:12 +0700

    fixing register UI

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,13 +14,8 @@
 
 	</head>
 	<body>
-	<section class="ftco-section">
+	<section class="ftco-section" style="padding-top:3%">
 		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Business Item Management</h2>
-				</div>
-			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-12 col-lg-10">
 					<div class="wrap d-md-flex">
